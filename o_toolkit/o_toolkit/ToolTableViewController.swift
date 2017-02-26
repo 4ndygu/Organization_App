bc633e6cb2cbf24560bd85431c8378e815732020
gammieduncan <dgammie@usc.edu>
2017-02-25 17:27:18 -0800

    Has method to read 'actions' database and print it

---
+++ o_toolkit/o_toolkit/ToolTableViewController.swift
@@ -0,0 +1,159 @@
+//
+//  ToolTableViewController.swift
+//  o_toolkit
+//
+//  Created by Student on 2/18/17.
+//  Copyright © 2017 wow. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class ToolTableViewController: UITableViewController {
+    
+    var ref: FIRDatabaseReference!
+    
+   // let oddNumbers = [];
+    
+    func loadOddNumbers()  {
+        
+
+        let oddNumbers = [String]()
+        ref = FIRDatabase.database().reference()
+        
+        ref = ref.child("actions")
+        
+        ref.observeSingleEvent(of: .value, with: {
+            snapshot in
+                print(snapshot.value as Any)
+//                oddNumbers.append(snapshot.value as String)
+        })
+ 
+        
+        
+    
+}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        loadOddNumbers();
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.tableView.delegate = self;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1;
+    }
+
+<<<<<<< HEAD
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return oddNumbers.count;
+//    }
+//
+//    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "protocell", for: indexPath)
+//        
+//        // Configure the cell...
+//        let item = oddNumbers[indexPath.row]
+//        cell.textLabel?.text = item;
+//        return cell
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let row = indexPath.row
+//        print(oddNumbers[row])
+//        //CODE TO BE RUN ON CELL TOUCH
+//    }
+=======
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return oddNumbers.count;
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "protocell", for: indexPath)
+        
+        // Configure the cell...
+        let item = oddNumbers[indexPath.row]
+        cell.textLabel?.text = item;
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let row = indexPath.row
+        print(oddNumbers[row])
+        //CODE TO BE RUN ON CELL TOUCH
+        
+        
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        
+        let DashController = storyBoard.instantiateViewController(withIdentifier: "orgpage") as UIViewController
+        
+        self.present(DashController, animated:true, completion:nil)
+    }
+>>>>>>> 3f7729535fcc74acd3ba1349a9a72172affc300a
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
