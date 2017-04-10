6a0c4e2146e75d2c6894fa3c2b6ef537f5540644
Student <Student@guest-wireless-upc-1600-10-120-005-149.usc.edu>
2017-04-09 20:45:46 -0700

    bring up to pual's version

--- o_toolkit/Pods/Target Support Files/Pods-o_toolkit/Pods-o_toolkit-frameworks.sh
+++ o_toolkit/Pods/Target Support Files/Pods-o_toolkit/Pods-o_toolkit-frameworks.sh
@@ -89,14 +89,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Braintree/Braintree.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/BraintreeDropIn/BraintreeDropIn.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Braintree/Braintree.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/BraintreeDropIn/BraintreeDropIn.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
 fi
